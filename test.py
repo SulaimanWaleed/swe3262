113b8e93abf91fa7f5fe74b47866c98ca2b74ca5
SulaimanWaleed <61086934+SulaimanWaleed@users.noreply.github.com>
2021-04-25 22:06:33 +0300

    Update test.py

--- test.py
+++ test.py
@@ -2,3 +2,8 @@
 f=open("/.deepsource.toml","r")
 f.write("config file.")
 f.close()
+
+
+def get_number(self,min_max=[1,10]):
+  assert all([isinstance(i,int) for i in min_max])
+  return random.randint(*min_max)
